9848a52b97cd97e4a3c4ee613c67404213755dab
Łukasz Filipek <l.filipek@o2.pl>
2022-07-20 01:23:00 +0200

    add Booking class

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
   
 const app = {
   initPages: function(){
@@ -83,6 +84,12 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+  initBooking: function(){
+    const thisApp = this;
+
+    const booking = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(booking);
+  },
   init: function(){
     const thisApp = this;
     //console.log('*** App starting ***');
@@ -93,6 +100,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
